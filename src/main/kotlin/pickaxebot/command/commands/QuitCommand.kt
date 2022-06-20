25f1e744ce808fba472c112169c628883470adb9
D <61399248+BotGUyz@users.noreply.github.com>
2022-06-20 17:16:08 +0700

    deleteUserData fix quitCommand - check user signed up - delete user data

--- src/main/kotlin/pickaxebot/command/commands/QuitCommand.kt
+++ src/main/kotlin/pickaxebot/command/commands/QuitCommand.kt
@@ -1,12 +1,15 @@
 package pickaxebot.command.commands
 
 import com.kotlindiscord.kord.extensions.components.components
+import dev.kord.rest.builder.message.create.embed
 import com.kotlindiscord.kord.extensions.components.publicButton
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
 import com.kotlindiscord.kord.extensions.types.respond
+import dev.kord.common.Color
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import pickaxebot.database.dao.UserManager
 import java.util.concurrent.TimeUnit
 
 class QuitCommand: Extension() {
@@ -20,20 +23,36 @@ class QuitCommand: Extension() {
 
             action {
                 respond {
-                    components {
-                        publicButton {
-                            label = "Confirm: DELETE ALL YOUR DATA AND QUIT"
-                            disabled = true
-                            withContext(Dispatchers.IO) {
-                                TimeUnit.SECONDS.sleep(5)
-                            }
-                            action {
-                                respond {
-                                    content = "Confirmed."
+                    if(UserManager.userExists(user.id)){
+                        components {
+
+                            publicButton {
+                                label = "Confirm: DELETE ALL YOUR DATA AND QUIT"
+                                disabled = true
+                                withContext(Dispatchers.IO) {
+                                    TimeUnit.SECONDS.sleep(5)
+                                }
+                                action {
+                                    UserManager.deleteUserData(user.id)
+                                    respond {
+                                        content = "Confirmed."
+                                    }
                                 }
                             }
                         }
+                    }   else {
+                        embed {
+                            title = "Unable to quit"
+                            description = "user quitting failed"
+                            color = Color(255, 0, 0)
+                            field {
+                                name = "Reason"
+                                value = "User isn't registered"
+                            }
+
+                        }
                     }
+
                 }
             }
         }
